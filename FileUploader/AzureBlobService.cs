b1daaa7dc746d7dc63170d1520b09a813ef755a8
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-08-18 14:44:57 +0100

    implement azure key vault connection

---
+++ FileUploader/AzureBlobService.cs
@@ -0,0 +1,124 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+
+namespace FileUploader
+{
+	public class AzureBlobService
+	{
+        // First Step
+        private readonly BlobServiceClient _blobServiceClient;
+
+
+		// Second Step
+        public AzureBlobService()
+		{
+            //var credential = new StorageSharedKeyCredential(_storageAccount, _accessKey);
+            //var blobUri = $"https://{_storageAccount}.blob.core.windows.net";
+            //_blobServiceClient = new BlobServiceClient(new Uri(blobUri), credential);
+
+            // Get the credentials from azure key vault
+            var client = new SecretClient(vaultUri: new Uri("https://kv-bookman-test-002.vault.azure.net/"), credential: new DefaultAzureCredential());
+			KeyVaultSecret storageAccount = client.GetSecret("azblobstorageaccount");
+            KeyVaultSecret accessKey = client.GetSecret("azblobaccesskey");
+
+			// Create the blob service client
+			var credential = new StorageSharedKeyCredential(storageAccount.Value, accessKey.Value);
+			var blobUri = $"https://{storageAccount.Value}.blob.core.windows.net";
+			_blobServiceClient = new BlobServiceClient(new Uri(blobUri), credential);
+        }
+
+		// List all the Blob Containers
+		public async Task ListBlobContainersAsync()
+		{
+			var containers = _blobServiceClient.GetBlobContainersAsync();
+			
+
+			await foreach (var container in containers)
+			{
+				Console.WriteLine(container.Name);
+			}
+		}
+
+		// Upload files Asynchronously
+		public async Task<List<Uri>> UploadFilesAsync()
+		{
+			var blobUris = new List<Uri>();
+			string fileName = "book-image-1.jpeg";
+			string filePath = Path.Combine($"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}/Projects/FileUploader/FileUploader", fileName);
+			var blobContainer = _blobServiceClient.GetBlobContainerClient("images");
+
+			var blob = blobContainer.GetBlobClient($"today/{fileName}");
+			var tomorrowBlob = blobContainer.GetBlobClient($"tomorrow/{fileName}");
+			Console.WriteLine(blob.Uri.AbsoluteUri);
+
+			await blob.UploadAsync(filePath, true);
+			blobUris.Add(blob.Uri);
+			await tomorrowBlob.UploadAsync(filePath, true);
+			blobUris.Add(tomorrowBlob.Uri);
+
+
+			return blobUris;
+		}
+
+		// Get flat list of blobs Asynchronously
+		public async Task GetFlatBlobsListAsync()
+		{
+			var blobContainer = _blobServiceClient.GetBlobContainerClient("images");
+			Console.WriteLine(blobContainer.Uri.AbsoluteUri);
+			var blobs = blobContainer.GetBlobsAsync();
+
+
+			await foreach (var blob in blobs)
+			{
+				Console.WriteLine("Blob name: {0}", blob.Properties.CopySource);
+			}
+		}
+
+		// Get Hierarchical list of blobs Asynchronously
+		public async Task GetHierarchicalBlobsListAsync()
+		{
+			var blobContainer = _blobServiceClient.GetBlobContainerClient("images");
+			var blobs = blobContainer.GetBlobsByHierarchyAsync();
+
+
+			await foreach (var blob in blobs)
+			{
+				if (blob.IsPrefix)
+				{
+					// Write out the prefix of the virtual directory.
+					Console.WriteLine("Virtual directory prefix: {0}", blob.Prefix);
+
+					// Call recursively with the prefix to traverse the virtual directory.
+					await GetHierarchicalBlobsListAsync();
+				}
+				else
+				{
+					// Write out the name of the blob.
+					Console.WriteLine("Blob name: {0}", blob.Blob.Name);
+				}
+			}
+		}
+
+		// Delete blob
+		public async Task DeleteBlobAsync()
+		{
+			string fileName = "book-image-1.jpeg";
+			var blobContainer = _blobServiceClient.GetBlobContainerClient("images");
+			var blob = blobContainer.GetBlobClient($"today/{fileName}");
+
+			bool deleteStatus = await blob.DeleteIfExistsAsync();
+
+			if (deleteStatus == true)
+			{
+				Console.WriteLine($"The file '{fileName}' was successfully deleted from Azure Storage blob.");
+			}
+			else
+			{
+				Console.WriteLine($"Something went wrong while trying to delete the file '{fileName}'.");
+			}
+		}
+	}
+}
+
---
+++ FileUploader/Program.cs
@@ -0,0 +1,60 @@
+﻿using FileUploader;
+
+var service = new AzureBlobService();
+await service.ListBlobContainersAsync();
+await service.UploadFilesAsync();
+await service.GetFlatBlobsListAsync();
+Console.WriteLine("===================");
+await service.GetHierarchicalBlobsListAsync();
+//await service.DeleteBlobAsync();
+
+
+//string fileName = "book-image.jpeg";
+//string filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+
+//Console.WriteLine($"Full path to the file: {filePath}");
+//Console.WriteLine(Directory.GetCurrentDirectory());
+//Console.WriteLine(AppContext.BaseDirectory);
+//Console.WriteLine(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location ?? string.Empty));
+//Console.WriteLine($"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}/Projects/FileUploader/FileUploader/book-image-1.jpeg");
+
+
+
+/// Key vault
+
+// Create a new secret client using the default credential from Azure.Identity using environment variables previously set,
+// including AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID.
+//var client = new SecretClient(vaultUri: new Uri("https://kv-bookman-test-002.vault.azure.net/"), credential: new DefaultAzureCredential());
+
+// Create a new secret using the secret client.
+//KeyVaultSecret secret = client.SetSecret("secret-name", "secret-value");
+
+// Retrieve a secret using the secret client.
+//secret = client.GetSecret("secret-name");
+//KeyVaultSecret storageAccount = await client.GetSecretAsync("azblobstorageaccount");
+//KeyVaultSecret accessKey = await client.GetSecretAsync("azblobaccesskey");
+
+//Console.WriteLine($"{storageAccount.Name}: {storageAccount.Value}");
+//Console.WriteLine($"{accessKey.Name}: {accessKey.Value}");
+
+
+
+//Console.WriteLine(secret.Name);
+//Console.WriteLine(secret.Value);
+//Console.WriteLine(secret.Properties.Version);
+//Console.WriteLine(secret.Properties.Enabled);
+
+//var keyVaultUri = new Uri("https://kv-bookman-test-002.vault.azure.net/");
+//var azureCredential = new DefaultAzureCredential();
+//var secretClient = new SecretClient(keyVaultUri, azureCredential);
+//var storageAccount = secretClient.GetSecret("azstorageblobaccount");
+//var accessKey = secretClient.GetSecret("azaccesskey");
+
+
+//Console.WriteLine($"Storage Account: ", storageAccount.Value);
+//Console.WriteLine($"Access Key: ", accessKey.Value);
+
+
+//Console.WriteLine("Hello, World!");
+
